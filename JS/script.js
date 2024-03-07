f643ddd9425917c393528450ebdb29e92d85dc48
Yaroslav Boikov <yaribum@mail.ru>
2024-03-07 22:12:43 +0200

    Desctop 0.3

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,36 @@
 const numCarouselLeft = document.getElementById('num_carousel_left');
 const numCarouselRight = document.getElementById('num_carousel_right');
-const btnCarouselLeft = document.getElementById('button_left_arrow');
+const btnCarouselLeft = document.querySelector('.button_left_arrow');
 const btnCarouselRight = document.querySelector('.button_right_arrow');
+const btnCarouselDisable = document.querySelector('.button_left_arrow_disable');
+const btnCarouselDisableRight = document.querySelector('.button_right_arrow_disable');
+const carouselItems = document.querySelectorAll('.main__content__participant_carousel_item');
 
-if (numCarouselLeft.innerText == 3) {
+
+btnCarouselLeft.addEventListener('click', () => {
+    numCarouselLeft.innerText = 3;
+    btnCarouselRight.classList.remove('btn-display-none');
+    btnCarouselLeft.classList.add('btn-display-none');
+    btnCarouselDisableRight.classList.add('btn-display-none');
+    btnCarouselDisable.classList.remove('btn-display-none');
     numCarouselRight.style.color = 'gray';
-} else {
-    numCarouselLeft.style.color = 'black'
-}
+
+    carouselItems.forEach((elem) => {
+        elem.style.transform = 'translateX(0%)'
+        elem.style.transition = 'all .6s ease'
+    });
+});
+
+btnCarouselRight.addEventListener('click', () => {
+    numCarouselLeft.innerText = 6;
+    btnCarouselRight.classList.add('btn-display-none');
+    btnCarouselDisableRight.classList.remove('btn-display-none');
+    btnCarouselDisable.classList.add('btn-display-none');
+    numCarouselRight.style.color = 'black';
+    btnCarouselLeft.classList.remove('btn-display-none');
+    carouselItems.forEach((elem) => {
+        elem.style.transform = 'translateX(-301%)'
+        elem.style.transition = 'all .6s ease'
+    });
+});
+
